76e04570fa878d9bb764a37f2ff1afbda61abe1f
Michael Nobles <Mnobles33@gmail.com>
2025-03-29 09:59:07 -0700

    next commit

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -19,6 +19,7 @@ const create = async account => {
 }
 
 const updateById = async (id, account) => {
+  // update accounts set name='foo', budget=1000 where id = 1;
   await db('accounts').where({ id }).update(account)
   return getById(id)
 }
