7dbc4e303b49ea9c3685deef89dff7725c8f2371
Michael Nobles <Mnobles33@gmail.com>
2025-03-29 08:43:13 -0700

    Next middleware done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,9 +1,41 @@
 const Account = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checkAccountPayload middleware')
-  next()
+  try {
+    if (!req.body.name || !req.body.budget) {
+      throw {
+        status: 400,
+        message: 'name and budget are required',
+      }
+    }
+    if (typeof req.body.name !== 'string') {
+      throw {
+        status: 400,
+        message: 'name must be a string',
+      }
+    }
+    if (typeof req.body.name !== 'string' || req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
+      throw {
+        status: 400,
+        message: 'name must be a string between 3 and 100 characters',
+      }
+    }
+    if (typeof req.body.budget !== 'number') {
+      throw {
+        status: 400,
+        message: 'budget must be a number',
+      }
+    }
+    if (typeof req.body.budget !== 'number' || req.body.budget < 0 || req.body.budget > 1000000) {
+      throw {
+        status: 400,
+        message: 'budget of account is too large or too small',
+      }
+    }
+    next()
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
